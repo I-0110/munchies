6d662f97dc192ab441858bbf712a0055ea128716
John Goldade <goldade.john@gmail.com>
2025-05-28 14:49:15 -0500

    Updated/ added models for the db structure (#4)

---
+++ server/src/models/Ingredient.ts
@@ -0,0 +1,34 @@
+import { Schema, model, Document } from 'mongoose';
+
+// Define an interface for the Profile document
+interface IIngredient extends Document {
+  _id: string;
+  name: string;
+  calories: number;
+}
+
+// Define the schema for the Profile document
+const ingredientSchema = new Schema<IIngredient>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    calories: {
+      type: Number,
+      required: true,
+      trim: true
+    }
+  },
+  {
+    timestamps: false,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+  }
+);
+
+const Ingredient = model<IIngredient>('Ingredients', ingredientSchema);
+
+export default Ingredient;
