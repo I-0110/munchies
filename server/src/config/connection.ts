ccaf4d2ec3b555af9de3b7384e8a5fd26a352ed6
John Goldade <goldade.john@gmail.com>
2025-05-28 21:33:44 -0500

    Feature deploy fix (#19)
    * Fixing deploy failure
    
    * Added fixes for resolvers and scripts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,19 +1,28 @@
-import dotenv from 'dotenv';
+// import the needed packages
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 dotenv.config();
+console.log(process.env.MONGODB_URI)
+// verify that a MONGODB_URI exists
+if (!process.env.MONGODB_URI) {
+    throw new Error('Environment variable MONGODB_URI is not defined');
+}
 
-import mongoose from 'mongoose';
+// variable to store the mongodb uri
+const MONGODB_URI: string = process.env.MONGODB_URI;
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
-
+// function to run the database connection 
 const db = async (): Promise<typeof mongoose.connection> => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('Database connected.');
-    return mongoose.connection;
-  } catch (error) {
-    console.error('Database connection error:', error);
-    throw new Error('Database connection failed.');
-  }
-};
+    try {
+        await mongoose.connect(MONGODB_URI)
+        console.log("🗄️ Database connected!")
+        return mongoose.connection
+    } catch (r) {
+        console.error('Error connecting to the database', r)
+        throw new Error ('DB connection failed 😩')
+    }
+}
 
+// export the db connection function for use by the server
 export default db;
+
