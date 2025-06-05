d3bab43dfb09e726bfe3e98f1e2d2200bc2ba3cc
John Goldade <goldade.john@gmail.com>
2025-06-04 21:39:03 -0500

    Feature save recipe (#65)
    * update client/utils/mutations
    
    * Completed client/src/utils/mutations.ts
    
    * Fixed deploy
    
    * Added logo to the top of the website
    
    * Signup and Login working!
    
    * add recipe model
    
    * Fixed!
    
    * Added a save recipe function to the recipe page
    
    * Made a fix to the client mutations
    
    * Fixing bug
    
    * adding button back in
    
    ---------
    
    Co-authored-by: I-0110 <ivelisbecker@gmail.com>
    Co-authored-by: blintgen <111295995+blintgen@users.noreply.github.com>
    Co-authored-by: Jpeterson <peterson.jacob13@protonmail.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,30 @@
 import { useState } from 'react';
 import SearchInput from '../components/Search';
 import MealCard from '../components/MealCard';
-
-
-import { QUERY_USERS } from '../utils/queries';
-import { useQuery } from '@apollo/client';
-import { Recipe } from '../utils/models/Recipe';
-import { retreiveTMDBRecipies } from '../utils/API/mealsAPI';
-
+// import UserList from '../components/UserList';
+// import { QUERY_USERS } from '../utils/queries';
+// import { useQuery, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { ADD_RECIPE } from '../utils/mutations';
 import { convertToRecipe } from '../utils/models/Recipe'
 
+type Meal = {
+  idMeal: string;
+  strMeal: string;
+  strCategory: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strYoutube: string;
+};
+
+type MealAPIResponse = {
+  meals: Meal[] | null;
+}
+
 const Home = () => {
 
-  const { loading: userLoading } = useQuery(QUERY_USERS);
-
   const [query, setQuery] = useState('');
-  const [result, setResult] = useState<Recipe[] | null>(null);
+  const [result, setResult] = useState<MealAPIResponse | null>(null);
   const [searchLoading, setSearchLoading] = useState(false);
   
   const [addRecipe] = useMutation(ADD_RECIPE)
@@ -24,13 +32,18 @@ const Home = () => {
   const handleSearch = async () => {
     setSearchLoading(true);
     try {
-      const response = await retreiveTMDBRecipies(query); 
-      const data = await response;
-      
-      setResult(data);
-      
-      console.log(`API response:`, data);
+      const response = await fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(query)}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } 
 
+      const returnedList = await response.json();
+      console.log(returnedList)
+      const recipeList = returnedList.meals ? returnedList.meals.map(convertToRecipe) : null;
+      setResult({ meals: recipeList });
+      console.log(`API response:`, recipeList
+
+      );
     } catch (error) {
       console.error('Fetch error:', error);
     } finally {
@@ -69,7 +82,7 @@ const Home = () => {
   };
 
   // const users = data?.users || [];
-  
+
   return (
     <main>
       <div className="flex-row justify-center">
@@ -81,22 +94,45 @@ const Home = () => {
             <SearchInput value={query} onChange={setQuery} handleSearch={handleSearch}  />
 
             {result ? (
-              Array.isArray(result) ? (
+              Array.isArray(result.meals) ? (
               <div>
                 <h3>Recipes:</h3>
-                <div className='meal-list'>{result.map((meal: any) => (
-                  <MealCard 
-                    key={meal.idMeal}
-                    _id={meal.idMeal} 
-                    name={meal.strMeal}
-                    category={meal.strCategory}
-                    instructions={meal.strInstructions}
-                    image_url={meal.strMealThumb}
-                    video_url={meal.strYoutube}
-                    ingredients={[]}
-                  />
-                ))}
-
+                <div className='meal-list'>{result.meals.map((meal: any) => (
+                  <div key={meal.idMeal}>
+                    <MealCard 
+                      _id={meal.idMeal} 
+                      name={meal.strMeal}
+                      category={meal.strCategory}
+                      instructions={meal.strInstructions}
+                      image_url={meal.strMealThumb}
+                      video_url={meal.strYoutube}
+                      ingredients={[]}
+                    />
+                    <select 
+                      name="choice" 
+                      multiple={false} 
+                      onChange={(e) => handleSave(
+                      meal.idMeal, 
+                      meal.strMeal, 
+                      meal.strCategory, 
+                      meal.strInstructions, 
+                      meal.strMealThumb, 
+                      meal.strYoutube, 
+                      meal.ingredients, 
+                      (e.target as HTMLSelectElement).value
+                      )}
+                    >
+                      <option value="" >Choose a Day to Save</option>
+                      <option value="sunday">Sunday</option>
+                      <option value="monday">Monday</option>
+                      <option value="tuesday">Tuesday</option>
+                      <option value="wednesday">Wednesday</option>
+                      <option value="thursday">Thursday</option>
+                      <option value="friday">Friday</option>
+                      <option value="saturday">Saturday</option>
+                    </select>
+                  </div>
+                  ))}
                 </div>
               </div>
               ) : (
@@ -112,3 +148,5 @@ const Home = () => {
 };
 
 export default Home;
+
+
