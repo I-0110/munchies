aa29e8e73822afc572e72a326c29e1c756e22b2b
Jpeterson <peterson.jacob13@protonmail.com>
2025-06-07 14:59:31 -0500

    styling header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -8,17 +8,17 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-header pb-4 flex border-b-10 border-accent shadow-xl shadow-accent">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+    <header className="w-screen bg-header flex border-b-10 border-accent shadow-xl shadow-accent">
+      <div className="w-screen container flex-column justify-space-between-lg justify-center align-center text-center">
         <Link className="text-sky-200 text-light" to="/">
           <h1 className="m-0 text-background" style={{ fontSize: '3rem' }}>
             Munchies
           </h1>
         </Link>
-        <h2 className="text-background" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+        <h2 className="hidden md:block text-background mb-5" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
           Plan your next meal. One meal at the time!
         </h2>
-        <div>
+        <div className='w-full flex justify-end pr-3'>
           {Auth.loggedIn() ? (
             <>
               <Link className="btn btn-lg btn-primary m-2" to="/me">
@@ -30,10 +30,10 @@ const Header = () => {
             </>
           ) : (
             <>
-              <Link className="bg-button text-accent px-4 py-2 rounded-lg" to="/login">
+              <Link className="bg-button text-background px-4 py-2 rounded-t-lg border-2 border-accent inset-shadow-sm/80 inset-shadow-accent focus:bg-button-focus hover:bg-button-dark" to="/login">
                 Login
               </Link>
-              <Link className="btn btn-lg btn-light m-2 bg-button" to="/signup">
+              <Link className="bg-button text-background px-4 py-2 rounded-t-lg border-2 border-accent inset-shadow-sm/80 inset-shadow-accent focus:bg-button-focus hover:bg-button-dark" to="/signup">
                 Register
               </Link>
             </>
